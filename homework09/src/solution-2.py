711fcf9ec9dc8b3d2f3a854e151e3c6e44915139
dooyeong20 <noviced22@gmail.com>
2021-04-06 20:25:16 +0900

    feat: Homework 09 -2
    Done homework 09 -2

---
+++ homework09/src/solution-2.py
@@ -0,0 +1,18 @@
+def solution(money):
+    answer = 0
+    length = len(money)
+
+    dp_1 = [0 for _ in range(length+1)]
+    dp_1[1] = money[0]
+    dp_1[2] = money[0]
+
+    for i in range(3, length):
+        dp_1[i] = max(dp_1[i-1], dp_1[i-2]+money[i-1])
+
+    dp_2 = [0 for _ in range(length+1)]
+    dp_2[2] = money[1]
+
+    for i in range(3, length+1):
+        dp_2[i] = max(dp_2[i-1], dp_2[i-2]+money[i-1])
+
+    return max(dp_1[length-1], dp_2[length])
